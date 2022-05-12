9564026b4d996c4464f8abc3b47853c0e8f78b27
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-05-12 13:34:52 +0300

    Update install-kubectl.sh

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -5,8 +5,10 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 sudo apt-get update
-sudo apt-get install -y kubectl
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
