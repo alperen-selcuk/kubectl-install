ace4c2f378aa13691c7464c01ac01c051f507f1b
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-10-13 16:16:24 +0300

    Update and rename install-kubectl.sh to install-kubectl-helm.sh

--- install-kubectl-helm.sh
+++ install-kubectl-helm.sh
@@ -12,3 +12,9 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo apt-get update
 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+sudo chmod 700 get_helm.sh
+
+./get_helm.sh
